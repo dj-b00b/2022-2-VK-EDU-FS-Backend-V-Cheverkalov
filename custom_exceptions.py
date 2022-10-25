64f9778d93f49b95c566fa33951801adecb958c6
dj-b00b <101154570+dj-b00b@users.noreply.github.com>
2022-10-25 19:30:06 +0300

    Delete custom_exceptions.py

--- custom_exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class CellOcupiedError(Exception):
-    """Класс исключения, отображающего, что выбранная клетка занята"""
-
-    def __init__(self, *args):
-        self.message = args[0] if args else None
-
-    def __str__(self):
-        return f'Ошибка: {self.message}'
