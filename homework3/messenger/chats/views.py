28aed56c562c36535329e9701a6bd5d3baafb460
dj-b00b <101154570+dj-b00b@users.noreply.github.com>
2022-10-30 16:36:07 +0300

    Update views.py

--- homework3/messenger/chats/views.py
+++ homework3/messenger/chats/views.py
@@ -11,16 +11,23 @@ def chat_list(request):
     return JsonResponse({'chats': chats})
 
 
+
 @require_POST
 def chat_create(request, num):
     check_exist_chat = list(map(lambda x: x['id'] != num, chats))
     if False not in check_exist_chat:
-        chats.append({'id': num, 'title': f'chat {num}',
-                     'messages': 0, 'users': ['vova', 'katya']})
+        chats.append({
+            'id': num, 
+            'title': f'chat {num}',
+            'messagesq': 0, 
+            'users': ['vova', 'katya']
+        })
     return JsonResponse({'create_chat': chats})
 
 
 @require_GET
 def chat_page(request, chat_id):
     requested_chat = [i for i in range(len(chats)) if chats[i]['id'] == chat_id]
-    return JsonResponse({'chat': chats[requested_chat[0]]})
+    if not requested_chat:
+        return HttpResponseNotFound()
+    return JsonResponse({'chat': chats[requested_chat[0]]}) 
